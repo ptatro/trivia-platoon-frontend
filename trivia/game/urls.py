cd603a50064ae56c8cb2148cc222fe748ef881f6
johnlillge <81765408+johnlillge@users.noreply.github.com>
2021-09-16 19:55:47 -0500

    Answers serializer, viewset, and route

--- trivia/game/urls.py
+++ trivia/game/urls.py
@@ -1,5 +1,5 @@
 from rest_framework_nested import routers
-from .views import GamesViewSet, QuestionsViewSet, ResultsViewSet
+from .views import GamesViewSet, QuestionsViewSet, ResultsViewSet, AnswersViewSet
 from django.urls import include
 from django.conf.urls import url
 
@@ -9,9 +9,12 @@ result_router = routers.NestedSimpleRouter(game_router, r"games", lookup="game")
 result_router.register(r"results", ResultsViewSet, basename="game-results")
 question_router = routers.NestedSimpleRouter(game_router, r"games", lookup="game")
 question_router.register(r"questions", QuestionsViewSet, basename="game-questions")
+answer_router = routers.NestedSimpleRouter(question_router, r"questions", lookup="question")
+answer_router.register(r"answers", AnswersViewSet, basename="question-answers")
 
 urlpatterns = [
     url(r"^", include(game_router.urls)),
     url(r"^", include(result_router.urls)),
     url(r"^", include(question_router.urls)),
+    url(r"^", include(answer_router.urls))
 ]
