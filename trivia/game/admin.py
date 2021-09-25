57d83ed50ab246e096070f747abae381e59e4526
rbnsngrgg <54326272+rbnsngrgg@users.noreply.github.com>
2021-09-25 11:14:11 -0400

    Delete trivia directory

--- trivia/game/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Game, Result, Answer, Question
-
-admin.site.register(Game)
-admin.site.register(Result)
-admin.site.register(Answer)
-admin.site.register(Question)
